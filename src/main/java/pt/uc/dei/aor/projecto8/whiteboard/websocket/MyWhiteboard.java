cd56ce34bdd8d38857552a4719198a2935335601
Pedro Pamplona <pcrpamplona@gmail.com>
2014-05-20 11:13:03 +0100

    app44

---
+++ src/main/java/pt/uc/dei/aor/projecto8/whiteboard/websocket/MyWhiteboard.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pt.uc.dei.aor.projecto8.whiteboard.websocket;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ *
+ * @author User
+ */
+@ServerEndpoint(value = "/whiteboardendpoint", encoders = {FigureEncoder.class}, decoders = {FigureDecoder.class})
+public class MyWhiteboard {
+
+    private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
+
+    @OnMessage
+    public void broadcastFigure(Figure figure, Session session) throws IOException, EncodeException {
+        System.out.println("broadcastFigure: " + figure);
+
+        for (Session peer : peers) {
+            if (!peer.equals(session)) {
+                peer.getBasicRemote().sendObject(figure);
+            }
+        }
+    }
+
+    @OnOpen
+    public void onOpen(Session peer) {
+        peers.add(peer);
+    }
+
+    @OnClose
+    public void onClose(Session peer) {
+        peers.remove(peer);
+    }
+
+    @OnMessage
+    public void broadcastSnapshot(ByteBuffer data, Session session) throws IOException {
+        System.out.println("broadcastBinary: " + data);
+        for (Session peer : peers) {
+            if (!peer.equals(session)) {
+                peer.getBasicRemote().sendBinary(data);
+            }
+        }
+    }
+}
---
+++ src/main/webapp/websocket.js
@@ -0,0 +1,37 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var wsUri = "ws://" + document.location.host + document.location.pathname + "whiteboardendpoint";
+var websocket = new WebSocket(wsUri);
+
+websocket.binaryType = "arraybuffer";
+
+websocket.onerror = function(evt) { onError(evt) };
+
+function onError(evt) {
+    writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+}
+
+websocket.onmessage = function(evt) { onMessage(evt) };
+
+function sendText(json) {
+    console.log("sending text: " + json);
+    websocket.send(json);
+}
+                
+function onMessage(evt) {
+    console.log("received: " + evt.data);
+    if (typeof evt.data == "string") {
+        drawImageText(evt.data);
+    } else {
+        drawImageBinary(evt.data);
+    }
+}
+
+function sendBinary(bytes) {
+    console.log("sending binary: " + Object.prototype.toString.call(bytes));
+    websocket.send(bytes);
+}
