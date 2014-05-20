27dee59e45194351d4f51168fca7ed0f146d709f
Pedro Pamplona <pcrpamplona@gmail.com>
2014-05-20 11:41:59 +0100

    app44

--- src/main/sql/script.sql
+++ src/main/sql/script.sql
@@ -2,32 +2,48 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+DROP SCHEMA IF EXISTS `jdbcrealm` ;
 CREATE SCHEMA IF NOT EXISTS `jdbcrealm` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
 USE `jdbcrealm` ;
 
 -- -----------------------------------------------------
--- Table `jdbcrealm`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `jdbcrealm`.`users` (
-  `username` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`username`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC));
-
-
--- -----------------------------------------------------
 -- Table `jdbcrealm`.`groups`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `jdbcrealm`.`groups` ;
+
 CREATE TABLE IF NOT EXISTS `jdbcrealm`.`groups` (
   `groupid` INT NOT NULL,
   `groupname` VARCHAR(255) NOT NULL,
   `username` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`groupid`));
+  PRIMARY KEY (`groupid`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC));
+
+
+-- -----------------------------------------------------
+-- Table `jdbcrealm`.`users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `jdbcrealm`.`users` ;
+
+CREATE TABLE IF NOT EXISTS `jdbcrealm`.`users` (
+  `username` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `userid` INT NOT NULL,
+  `groups_groupid` INT NOT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
+  INDEX `fk_users_groups1_idx` (`groups_groupid` ASC),
+  CONSTRAINT `fk_users_groups1`
+    FOREIGN KEY (`groups_groupid`)
+    REFERENCES `jdbcrealm`.`groups` (`groupid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
 -- Table `jdbcrealm`.`whiteboard`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `jdbcrealm`.`whiteboard` ;
+
 CREATE TABLE IF NOT EXISTS `jdbcrealm`.`whiteboard` (
   `whiteboard_id` INT NOT NULL,
   `name` VARCHAR(255) NOT NULL,
